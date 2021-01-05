0a4f0aa38d387cf383cc272459a76eb6c461659e
Kenji Hisazumi <hisazumi@gmail.com>
2021-01-06 00:35:57 +0900

    we can run python3 and pytorch 1.7.1

---
+++ src/exec.sh
@@ -0,0 +1,3 @@
+PARENT="../models_and_pretrained_networks"
+DIR="$PARENT/9. datatypes of different sizes (from 1 to 10)/"
+python3 translate.py --train_data="$DIR/models_train.json" --val_data="$DIR/models_valid.json" --test_data="$DIR/models_test.json"
